2271314cea5a26964ecfb4c01a72af81581381bf
Brandon Weaver <a7xncob@gmail.com>
2020-01-03 00:01:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require './environment'
 
 module FormsLab
+  
   class App < Sinatra::Base
-
-    # code other routes/actions here
-
+    
+    get '/' do
+      erb :root
+    end
+    
+    get "/new" do
+      erb :"pirates/new"
+    end
+    
+    post "/pirates" do
+      
+    end
+    
   end
+  
 end
